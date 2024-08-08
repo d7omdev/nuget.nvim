6e7c61f6c854ba002bb5886be7f0b91143776f8d
abdulrahmanDev1 <abdohamdy198461@gmail.com>
2024-08-08 14:25:02 +0300

    update kemaps handling logic

--- lua/nuget/init.lua
+++ lua/nuget/init.lua
@@ -22,29 +22,35 @@ end, {})
 -- Function to setup keymaps
 function nuget.setup(opts)
 	opts = opts or {}
-	opts.keys = opts.keys or {}
 
-	if next(opts.keys) == nil then
-		-- Use default keymaps if none provided
+	-- If no keys are provided, use default keymaps
+	if opts.keys == nil then
 		opts.keys = default_keys
 	end
 
-	if opts.keys.install then
-		vim.api.nvim_set_keymap(
-			opts.keys.install[1],
-			opts.keys.install[2],
-			"<cmd>NuGetInstall<CR>",
-			{ noremap = true, silent = true, desc = "Install a NuGet package" }
-		)
-	end
+	-- Disable keymaps if an empty keys table is provided
+	if next(opts.keys) == nil then
+		vim.api.nvim_del_keymap("n", default_keys.install[2])
+		vim.api.nvim_del_keymap("n", default_keys.remove[2])
+	else
+		-- Set provided keymaps or default keymaps
+		if opts.keys.install then
+			vim.api.nvim_set_keymap(
+				opts.keys.install[1],
+				opts.keys.install[2],
+				"<cmd>NuGetInstall<CR>",
+				{ noremap = true, silent = true, desc = "Install a NuGet package" }
+			)
+		end
 
-	if opts.keys.remove then
-		vim.api.nvim_set_keymap(
-			opts.keys.remove[1],
-			opts.keys.remove[2],
-			"<cmd>NuGetRemove<CR>",
-			{ noremap = true, silent = true, desc = "Remove a NuGet package" }
-		)
+		if opts.keys.remove then
+			vim.api.nvim_set_keymap(
+				opts.keys.remove[1],
+				opts.keys.remove[2],
+				"<cmd>NuGetRemove<CR>",
+				{ noremap = true, silent = true, desc = "Remove a NuGet package" }
+			)
+		end
 	end
 end
 
